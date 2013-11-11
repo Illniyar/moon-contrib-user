ffb5275a20c0acad2f03f500296f30be0d7370b5
Alon <Illniyar@gmail.com>
2013-11-11 09:53:25 +0200

    updated to support moonshine 0.0.5 and added tests

---
+++ test/authentication.client.js
@@ -0,0 +1,26 @@
+var expect = chai.expect
+describe("authentication service",function(){
+    beforeEach(angular.mock.module('user.authentication',function(authenticationProvider){
+        authenticationProvider.registerAuthenticatior("success",function(authInfo,cb){
+            setTimeout(function(){cb(null,{authInfo:authInfo,success:true})},0)
+        })
+        authenticationProvider.registerAuthenticatior("failure",function(authInfo,cb){
+            setTimeout(function(){cb(new Error("don't log in"))},0)
+        })
+    }))
+    it("should login authenticated user",function(done){
+        inject(function(authentication,$httpBackend){
+            $httpBackend.expectGET("/api/v1/users/me")
+                .respond({
+                    result:"yay!"
+                })
+            authentication.authenticate("success",{text:"new user"})
+                .then(function(userInfo){
+                    expect(userInfo.authInfo.text).to.equal("new user")
+                    expect(authentication.user.authInfo.text).to.equal("new user")
+                    expect(authentication.isAuthenticated).to.equal(true)
+                    done()
+                },done)
+        })
+    })
+})
